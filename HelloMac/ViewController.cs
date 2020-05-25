5ffbd7d933f75a6ee5352a597d33bf80499d9c5f
chris-schmitz <schmitz.chris@gmail.com>
2020-05-25 09:07:02 -0500

    Works through HelloMac tutorial for building native mac apps using c#
    https://docs.microsoft.com/en-us/xamarin/mac/get-started/hello-mac#creating-the-interface

---
+++ HelloMac/ViewController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using AppKit;
+using Foundation;
+
+namespace HelloMac
+{
+    public partial class ViewController : NSViewController
+    {
+        private int numberOfTimesClicked = 0;
+
+
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            ClickLabel.StringValue = "The button has not been clicked yet.";
+
+            // Do any additional setup after loading the view.
+        }
+
+        partial void ClickedButton(NSObject sender)
+        {
+            ClickLabel.StringValue = string.Format("The button has been clicked {0} time{1}.", ++numberOfTimesClicked, (numberOfTimesClicked < 2) ? "" : "s");
+        }
+
+        public override NSObject RepresentedObject
+        {
+            get
+            {
+                return base.RepresentedObject;
+            }
+            set
+            {
+                base.RepresentedObject = value;
+                // Update the view, if already loaded.
+            }
+        }
+    }
+}
